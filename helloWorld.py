9dbb0a23e565220f2296fbeda5c92cff3b8c691e
leungyp <leungyp@gmail.com>
2024-03-19 16:09:06 +0000

    T23-Practical Task 2

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,5 @@
+name1=input("Please input an user's name : ")
+print("The user's name is ",name1)
+age1=input("Please input an user's age : ")
+print("The user's age is ",age1)
 print("Git is awesome!")
